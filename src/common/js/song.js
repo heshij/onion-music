16d14e2e1ff6b8119a5a5ed17fe4b5c1e64244c3
heshij <1634943446@qq.com>
2018-12-16 16:17:27 +0800

    init

--- src/common/js/song.js
+++ src/common/js/song.js
@@ -1,4 +1,4 @@
-import {getLyric} from 'api/song'
+import {getLyric, getVkey} from 'api/song'
 import {ERR_OK} from 'api/config'
 import {Base64} from 'js-base64'
 
@@ -32,7 +32,7 @@ export default class Song {
   }
 }
 
-export function createSong(musicData) {
+export function createSong(musicData,url) {
   return new Song({
     id: musicData.songid,
     mid: musicData.songmid,
@@ -41,7 +41,8 @@ export function createSong(musicData) {
     album: musicData.albumname,
     duration: musicData.interval,
     image: `https://y.gtimg.cn/music/photo_new/T002R300x300M000${musicData.albummid}.jpg?max_age=2592000`,
-    url: `http://ws.stream.qqmusic.qq.com/${musicData.songid}.m4a?fromtag=46`
+    //url: `http://ws.stream.qqmusic.qq.com/${musicData.songid}.m4a?fromtag=46`
+    url: url
   })
 }
 
--- src/components/singer-detail/singer-detail.vue
+++ src/components/singer-detail/singer-detail.vue
@@ -1,6 +1,6 @@
 <template>
   <transition name="slide">
-    <div class="singer-detail"></div>
+    <music-list :title="title" :bg-image="bgImage" :songs="songs"></music-list>
   </transition>
 </template>
 
@@ -8,9 +8,25 @@
   import {mapGetters} from 'vuex'
   import {getSingerDetail} from "../../api/singer";
   import {ERR_OK} from "../../api/config";
+  import {createSong} from "../../common/js/song";
+  import {getVkey} from "../../api/song";
+  import MusicList from "../music-list/music-list";
 
   export default {
+    components: {MusicList},
+    data() {
+      return {
+        songs: [],
+        newSongs: []
+      }
+    },
     computed: {
+      title() {
+        return this.singer.name
+      },
+      bgImage() {
+        return this.singer.avatar
+      },
       ...mapGetters([
         'singer'
       ])
@@ -27,25 +43,41 @@
         }
         getSingerDetail(this.singer.id).then((res) => {
           if (res.code === ERR_OK) {
-            console.log(res.data.list)
+            this.songs = this._normalizeSongs(res.data.list)
+            // console.log(this.songs);
           }
         })
+      },
+      _normalizeSongs(list) {
+        list.forEach((item) => {
+          let {musicData} = item
+          if (musicData.songid && musicData.albummid) {
+            getVkey(musicData.songmid).then(res => {
+              if (res.code === ERR_OK) {
+                let data = res.data.items[0]
+                let url = `http://dl.stream.qqmusic.qq.com/${data.filename}?vkey=${data.vkey}&guid=7332953645&fromtag=66`
+                this.newSongs.push(createSong(musicData, url))
+              }
+            })
+          }
+        })
+        return this.newSongs
       }
     }
   }
 </script>
 
 <style scoped lang="stylus" rel="stylesheet/stylus">
- /* @import "~common/stylus/variable"
-  .singer-detail
-    position: fixed
-    z-index: 100
-    top: 0
-    left: 0
-    right: 0
-    bottom: 0
-    background: $color-background
-    */
+  /* @import "~common/stylus/variable"
+   .singer-detail
+     position: fixed
+     z-index: 100
+     top: 0
+     left: 0
+     right: 0
+     bottom: 0
+     background: $color-background
+     */
   .slide-enter-active, .slide-leave-active
     transition: all 0.3s
 
